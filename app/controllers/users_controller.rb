6237714d323070dd018498d8e6d4b07c90b2b62f
sco-bo <scottebobbitt@gmail.com>
2016-02-10 17:59:05 -0500

    adds form helpers and user edit capability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes user_params
+      flash[:notice] = 'The User is successfully updated'
+      redirect_to edit_user_path
+    else
+      render :edit 
+    end
+  end
+
+  private
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
